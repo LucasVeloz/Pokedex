0b751e82f4738e2003fb7a3c8c5194e482463ff8
LucasVeloz <lurucell@gmail.com>
2020-11-13 10:22:30 -0300

    trabalhando no header com botao e css com bug

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h4>Desenvolvido por Lucas Veloz</h4>
+        </div>
+    );
+}
+
+export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,28 @@
-import "../style/header.stylus";
+import { useEffect } from "react";
+
+// import "../style/header.stylus";
 const Header = () => {
     return (
 
         <div className="header">
             <h3>
-                <a href="#">POKEDEX</a>
+                <a href="/">POKEDEX</a>
             </h3>
             <ul>
                 <li>
-                    <a href="#">
+                        <button className='register'>Register</button>
+                    {/* <a href="#">
                         <div className='register'><p>Register</p></div>
-                    </a>
+                    </a> */}
                 </li>
                 <li>
-                    <a href="#">
+                    <button className='login'>Sign In</button>
+                    {/* <a href="#">
                         <div className='login'><p>Sign In</p></div>
-                    </a>
+                    </a> */}
                 </li>
             </ul>
         </div>
     );
 }
-
 export default Header;
