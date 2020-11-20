bef08dd7df86a5a7484d1201bebd12f334744ac3
LucasVeloz <lurucell@gmail.com>
2020-11-20 12:38:47 -0300

    magica do luis

--- src/components/list/index.js
+++ src/components/list/index.js
@@ -12,7 +12,6 @@ export default function List({ pokemons }) {
             const handleClose = () => setShow(false);
             const handleShow = () => setShow(true);
         
-            console.log(pokemon);
             return (
             <>
                 <Button variant="primary" onClick={handleShow} className='btn-espec'>
@@ -23,16 +22,16 @@ export default function List({ pokemons }) {
                 <Modal.Header >
                     <Modal.Title>
                         <h4>
-                            {pokemon.name}  
+                            {pokemon.pokemon.name}  
                         </h4>
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <img src={pokemon.image_url}></img>
-                    <h5>{pokemon.number}</h5>
-                    <h5>{pokemon.weight}</h5>
-                    <h5>{pokemon.height}</h5>
-                    <h5>{pokemon.kind}</h5>
+                    <img src={pokemon.pokemon.image_url} alt='imagem do pokemon'></img>
+                    <h5>{pokemon.pokemon.number}</h5>
+                    <h5>{pokemon.pokemon.weight}</h5>
+                    <h5>{pokemon.pokemon.height}</h5>
+                    <h5>{pokemon.pokemon.kind}</h5>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
@@ -46,21 +45,21 @@ export default function List({ pokemons }) {
 
     return (
         <>
-            {pokemons && pokemons.map((pokemons) => (
+            {pokemons && pokemons.map((pokemon) => (
                 // <Router>
                 //     <Link to={'/'}>
                 // <a href='/pokemons/'>
 
-                        <li key={pokemons.id}>
+                        <li key={pokemon.id}>
                             <div className='box'>
                                 <div className='head'>
-                                    <h5>#{pokemons.number}</h5>
-                                    <h2> {pokemons.name} </h2>
+                                    <h5>#{pokemon.number}</h5>
+                                    <h2> {pokemon.name} </h2>
                                 </div>
                                 <div className='pokemon'>
-                                    <img src={pokemons.image_url} alt='imagem do pokemon'></img>
+                                    <img src={pokemon.image_url} alt='imagem do pokemon'></img>
                                 </div>
-                                    <PokemonEspec props={pokemons} />
+                                    <PokemonEspec pokemon={pokemon} />
                             </div>
                         </li>
                 // </a>
--- src/pages/pokemons.js
+++ src/pages/pokemons.js
@@ -6,13 +6,44 @@ export default function Pokemons ({page}) {
     
     const [pokemons, setPokemons] = useState([]);
 
-    useEffect( () => {
-        axios
-            .get("https://pokedex20201.herokuapp.com/pokemons?page=1")
-            .then((response) => response.data.data)
-            .then((data) => setPokemons(data));
-    },[]);
+    // let total = [];
+    // function getPokemons (contador) {
+        
+    //     axios
+    //     .get("https://pokedex20201.herokuapp.com/pokemons?page=" + contador)
+    //     .then((response) => response.data.data)
+    //     .then((data) => {return(data)});
 
+    // }
+    useEffect ( () => {
+                
+        let total = [];
+        let promises = [];
+        for (let i = 1; i < 33; i++) {
+            promises.push(
+                axios.get(`https://pokedex20201.herokuapp.com/pokemons?page=` + i).then(response => {
+                // do something with response
+                let resposta = response.data.data;
+                total = total.concat(resposta);
+                
+            })
+            )
+        }
+        
+Promise.all(promises).then(() => {total.sort((a,b) =>{
+    
+        if (a.id > b.id) {
+        return 1;
+    }
+    if (a.id < b.id) {
+        return -1;
+    }
+    // a must be equal to b
+    return 0;
+}
+    );setPokemons(total)});
+        },[]);
+    
     // console.log({pokemons});
     return <PokemonsList pokemons={pokemons} />;
 }
